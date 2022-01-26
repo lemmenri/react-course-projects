023567fbd9b5818cfcc14a58ee54050ba890a75b
lemmenri <rick@joberi.nl>
2022-01-26 18:24:25 +0100

    89 - ES6 Array Destructuring

--- expensify-app/src/playground/desctructuring.js
+++ expensify-app/src/playground/desctructuring.js
@@ -1,3 +1,7 @@
+//
+// Object destructuring
+//
+
 // const person = {
 //     name: 'Rick',
 //     age: 32,
@@ -18,13 +22,25 @@
 // }
 
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-    }
-}
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//     }
+// }
 
-const { name: publisherName = 'Self-Published' } = book.publisher
+// const { name: publisherName = 'Self-Published' } = book.publisher
 
-console.log(publisherName)
+// console.log(publisherName)
+
+//
+// Array destructuring
+//
+
+const address = ['1299 S juniper Street', 'Philadelphia', 'Pennsylvania', '19147']
+const [, city, state = 'New York'] = address
+console.log(`You are in ${city} ${state}`)
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75']
+const [product, , price] = item
+console.log(`A medium ${product} costs ${price}`)
