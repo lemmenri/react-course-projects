ef426062dfda405a47d49905a53a34773de3c2b4
lemmenri <rick@joberi.nl>
2022-01-31 20:28:52 +0100

    104 - dropdown for picking sortby

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -13,7 +13,7 @@ const store = configureStore()
 
 store.dispatch(addExpense({ description: 'Water Bill', note: 'The bill for the water', amount: 5000, createdAt: 10000 }))
 store.dispatch(addExpense({ description: 'Gas Bill', note: 'The bill for the gas', amount: 15000, createdAt: 150000 }))
-store.dispatch(setTextFilter('water'))
+store.dispatch(addExpense({ description: 'Rent', note: 'The bill for the rent', amount: 1095, createdAt: 1000090 }))
 
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
