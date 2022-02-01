39b9393179eaa81dd11ed4adc31eb31fad972a35
lemmenri <rick@joberi.nl>
2022-02-01 17:11:29 +0100

    107 - wiring up add expense

--- expensify-app/src/components/AddExpensePage.js
+++ expensify-app/src/components/AddExpensePage.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { connect } from 'react-redux'
+import { useNavigate } from "react-router-dom";
 import ExpenseForm from "./ExpenseForm";
+import { addExpense } from '../actions/expenses'
 
-const AddExpensePage = () => (
-    <div>
-        <h1>Add Expense</h1>
-        <ExpenseForm />
-    </div>
-)
+const AddExpensePage = (props) => {
+    const navigate = useNavigate();
+    return (
+        <div>
+            <h1>Add Expense</h1>
+            <ExpenseForm
+                onSubmit={(expense) => {
+                    props.dispatch(addExpense(expense))
+                    navigate('/')
+                }}
+            />
+        </div>
+    )
+}
 
-export default AddExpensePage;
+export default connect()(AddExpensePage);
